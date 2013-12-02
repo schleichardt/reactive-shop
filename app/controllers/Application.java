a6003af76c66a80cbd7700af66eb9eb91fde130f
Michael Schleichardt <michael@schleichardt.net>
2013-12-02 23:03:08 +0100

    retrieve products

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,27 @@
 package controllers;
 
+import io.sphere.client.model.SearchResult;
+import io.sphere.client.shop.model.Product;
 import play.*;
+import play.libs.F;
+import play.libs.F.Promise;
 import play.mvc.*;
 
+import sphere.ShopController;
 import views.html.*;
 
-public class Application extends Controller {
+import java.util.List;
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+public class Application extends ShopController {
+
+    public static Promise<Result> index() {
+        final Promise<SearchResult<Product>> searchResult = sphere().products().all().fetchAsync();
+        return searchResult.map(new F.Function<SearchResult<Product>, Result>() {
+            @Override
+            public Result apply(SearchResult<Product> productSearchResult) throws Throwable {
+                final List<Product> products = productSearchResult.getResults();
+                return ok(index.render(products));
+            }
+        });
     }
-
 }
--- build.sbt
+++ build.sbt
@@ -9,3 +9,7 @@ libraryDependencies ++= Seq(
 )     
 
 play.Project.playJavaSettings
+
+libraryDependencies += "io.sphere" %% "sphere-play-sdk" % "0.48.0" withSources() exclude("org.scala-stm", "scala-stm_2.10.0") exclude("play", "play_2.10")
+
+templatesImport += "io.sphere.client.shop.model._"
