26face0d93d33219accf0505df2a0c42c4554ffa
Michael Schleichardt <michael@schleichardt.net>
2014-01-23 07:31:30 +0100

    Prepare template functions for async calls.

--- app/controllers/ReactiveShopController.java
+++ app/controllers/ReactiveShopController.java
@@ -5,6 +5,7 @@ import io.sphere.client.shop.model.Category;
 import play.api.mvc.Codec;
 import play.api.mvc.Results$;
 import play.api.templates.Html;
+import play.libs.F;
 import play.mvc.Content;
 import play.mvc.Result;
 import play.mvc.Results;
@@ -17,22 +18,24 @@ public class ReactiveShopController extends ShopController {
         return sphere().categories().getRoots(CategoryComparators.byName());
     }
 
-    protected static Result render(final Html html) {
-        return render(200, html);
-    }
 
-    protected static Result render(final int statusCode, final Html html) {
-        final Content content = new Content() {
+    protected static F.Promise<Result> render(final int statusCode, final F.Promise<Html> htmlPromise) {
+        return htmlPromise.map(new F.Function<Html, Result>() {
             @Override
-            public String body() {
-                return html.body();
-            }
+            public Result apply(final Html html) throws Throwable {
+                final Content content = new Content() {
+                    @Override
+                    public String body() {
+                        return html.body();
+                    }
 
-            @Override
-            public String contentType() {
-                return "text/html";
+                    @Override
+                    public String contentType() {
+                        return "text/html";
+                    }
+                };
+                return new Results.Status(Results$.MODULE$.Status(statusCode), content, Codec.utf_8());
             }
-        };
-        return new Results.Status(Results$.MODULE$.Status(statusCode), content, Codec.utf_8());
+        });
     }
 }
--- app/widgetpoc1/Controller.java
+++ app/widgetpoc1/Controller.java
@@ -4,6 +4,7 @@ import controllers.ReactiveShopController;
 import play.api.mvc.Codec;
 import play.api.mvc.Results$;
 import play.api.templates.Html;
+import play.libs.F;
 import play.mvc.Content;
 import play.mvc.Result;
 import play.mvc.Results;
@@ -15,13 +16,23 @@ public class Controller extends ReactiveShopController {
         public PageData(String title) {
             this.title = title;
         }
+
+        public F.Promise<PageData> promise() {
+            return F.Promise.pure(this);
+        }
     }
 
-    private static Result render(final Html html, final PageData pageData) {
-        return render(widgetpoc1.html.Controller.apply(html, pageData));
+    private static F.Promise<Result> render(final Html html, final PageData pageData) {
+        final F.Promise<Html> htmlPromise = pageData.promise().map(new F.Function<PageData, Html>() {
+            @Override
+            public Html apply(PageData pageData) throws Throwable {
+                return widgetpoc1.html.Controller.apply(html, pageData);
+            }
+        });
+        return render(200, htmlPromise);
     }
 
-    public static Result index() {
+    public static F.Promise<Result> index() {
         final PageData pageData = new PageData("the page title for index");
         return render(widgetpoc1.html.index.apply("additional data"), pageData);
     }
